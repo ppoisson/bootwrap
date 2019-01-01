5200bf6af9f06f97a8069aed4b60b2de10578367
ppoisson <paul.poisson@gmail.com>
2019-01-01 17:26:35 -0500

    cleaned up and refactored

--- bs_functions.php
+++ bs_functions.php
@@ -3,26 +3,26 @@
 function bs_container($content, $class='', $tag='div') {
 	$attr = "";
 	if($tag !== 'div') {
-		$attr .= " role=\"$tag\"";
+		$attr .= " role='$tag'";
 	}
 	if(isset($class)){
-		$class = " $class";
+		$class = " " . $class;
 	}
-	return base_el($tag, $content, "container$class", $attr);
+	return base_el($tag, $content, "container" . $class, $attr);
 }
 
 function bs_row($content, $class='') {
 	if(isset($class)){
-		$class = " $class";
+		$class = " " . $class;
 	}
-	return base_el("div", $content, "row$class");
+	return base_el("div", $content, "row" . $class);
 }
 
 function bs_col($content, $class='') {
 	if(isset($class)){
-		$class = " $class";
+		$class = " " . $class;
 	}
-	return base_el("div", $content, "col$class");
+	return base_el("div", $content, $class);
 }
 
 function bs_media($media) {
@@ -50,7 +50,7 @@ function bs_media($media) {
 		$class = $media['class'];
 	}
 	if(isset($media['heading'])){
-		$heading = "<h5 class=\"mt-0\">" . $media['heading'] . "</h5>";
+		$heading = "<h5 class='mt-0'>" . $media['heading'] . "</h5>";
 	}
 	if(isset($media['image']['class'])){
 		$image_class = $media['image']['class'];
@@ -63,7 +63,7 @@ function bs_media($media) {
 	$media_content = $media['content'];
 	$media_body = base_el("div", $heading . $media_content, "media-body");
 
-	return base_el($tag, $img . $media_body, "media$class");
+	return base_el($tag, $img . $media_body, "media" . $class);
 }
 
 function bs_media_list($media_list_arr, $image_class='mr-3') {
@@ -92,18 +92,18 @@ function bs_navs($items, $class='') {
 			$class = $item['class'];
 		}
 		if(isset($item['target'])){
-			$target = " target=\"" . $item['target'] . "\"";
+			$target = " target='" . $item['target'] . "'";
 		}
 		if(isset($item['href'])) {
 			$href = $item['href'];
-			$atag = base_a($content, $href, "nav-link $class", $target);
+			$atag = base_a($content, $href, "nav-link " . $class, $target);
 		} else {
-			$atag = base_el("span", $content, "nav-link $class");
+			$atag = base_el("span", $content, "nav-link " . $class);
 		}
 		
 		$li .= base_el("li", $atag, "nav-item");
 	}
-	return base_el("ul", $li, "nav $class");
+	return base_el("ul", $li, "nav " . $class);
 }
 
 function bs_button($button) {
@@ -129,24 +129,24 @@ function bs_button($button) {
 		$tag = $button['tag'];
 	}
 	if(isset($button['href'])) {
-		$attr .= " href=\"" . $button['href'] . "\"";
-		$attr .= " role=\"button\"";
+		$attr .= " href='" . $button['href'] . "'";
+		$attr .= " role='button'";
 	}
 	if(isset($button['class'])) {
-		$attr .= " class=\"$class " . $button['class'] . "\"";
+		$attr .= " class='" . $class . " " . $button['class'] . "'";
 	} else {
-		$attr .= " class=\"$class\"";
+		$attr .= " class='" . $class . "'";
 	}
 	if(isset($button['target'])) {
-		$attr .= " target=\"" . $button['target'] . "\"";
+		$attr .= " target='" . $button['target'] . "'";
 	}
 	if(isset($button['type'])) {
-		$attr .= " type=\"" . $button['type'] . "\"";
+		$attr .= " type='" . $button['type'] . "'";
 	}
 
 	if($button['isInput']) {
-		$attr .= " type=\"submit\"";
-		$attr .= " value=\"$content\"";
+		$attr .= " type='submit'";
+		$attr .= " value='" . $content . "'";
 		return "<input $attr>";
 	} else {
 		return base_el($tag, $content, $class, $attr);
@@ -208,15 +208,15 @@ function bs_card($card) {
 
 	if(isset($card['title'])) {
 		$card_title = $card['title'];
-		$card_body .= "<h5 class=\"card-title\">$card_title</h5>";
+		$card_body .= "<h5 class='card-title'>" . $card_title . "</h5>";
 	}
 	if(isset($card['subtitle'])) {
 		$card_subtitle = $card['subtitle'];
-		$card_body .= "<h6 class=\"card-subtitle\">$card_subtitle</h6>";
+		$card_body .= "<h6 class='card-subtitle'>" . $card_subtitle . "</h6>";
 	}
 	if(isset($card['text'])) {
 		$card_text = $card['text'];
-		$card_body .= "<p class=\"card-text\">$card_text</p>";
+		$card_body .= "<p class='card-text'>" . $card_text . "</p>";
 	}
 	if(isset($card['class'])) {
 		$card_class .= " " . $card['class'];
@@ -251,7 +251,7 @@ function bs_card($card) {
 	$content = $card_header;
 	$content .= base_el("div", $card_body, "card-body");
 	$content .= $card_footer;
-	return base_el("div", $content, "card$card_class");
+	return base_el("div", $content, "card" . $card_class);
 }
 
 function bs_card_layout($content, $layout='card-deck') {
@@ -263,23 +263,23 @@ function bs_card_layout($content, $layout='card-deck') {
 function base_a($content, $href, $class='', $target='') {
 	$attr = "";
 	if(isset($class)) {
-		$attr .= " class=\"$class\"";
+		$attr .= " class='" . $class . "'";
 	}
 	if(isset($target)) {
-		$attr .= " target=\"$target\"";
+		$attr .= " target='" . $target . "'";
 	}
-	return "<a href=\"$href\"$attr>$content</a>";
+	return "<a href='" . $href . "'" . $attr . ">" . $content . "</a>";
 }
 
 function base_img($src, $class, $alt) {
 	$attr = "";
 	if(isset($class)) {
-		$attr .= " class=\"$class\"";
+		$attr .= " class='" . $class . "'";
 	}
 	if(isset($alt)) {
-		$attr .= " alt=\"$alt\"";
+		$attr .= " alt='" . $alt . "'";
 	}
-	return "<img src=\"$src\"$attr>";
+	return "<img src='" . $src . "'" . $attr . ">";
 }
 
 function base_el($element, $content, $class='', $attrs='') {
@@ -288,9 +288,9 @@ function base_el($element, $content, $class='', $attrs='') {
 		$attr .= $attrs;
 	}
 	if(isset($class)) {
-		$attr .= " class=\"$class\"";
+		$attr .= " class='" . $class . "'";
 	}
-	return "<$element$attr>$content</$element>";
+	return "<" . $element . $attr . ">" . $content . "</" . $element . ">";
 }
 
 ?>
